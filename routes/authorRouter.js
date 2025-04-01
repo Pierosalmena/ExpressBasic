36d6ba29962226f4b0b7dbc5faeba718bc24c290
pierosalmena <piero.salmena@gmail.com>
2025-04-01 21:28:37 +0200

    Add author controller with mock DB and route integration
    Introduce a controller function to handle GET requests for a specific
    author by ID. The controller extracts the authorId from the route
    parameters, retrieves the corresponding data from a mock database, and
    returns a response accordingly.
    
    Also created a mock database file (`db.js`) with sample author data
    and integrated the controller with the `/authors/:authorId` route.
    
    This sets the foundation for applying MVC principles and allows for
    clean separation of concerns between routing and logic.

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,11 +1,14 @@
 const {Router} = require("express");
+const {getAuthorById} = require('../controllers/authorController')
 
 const authorRouter = Router();
 
 authorRouter.get("/", (req,res) => res.send("All authors"));
-authorRouter.get("/:authorId", (req, res) => {
-    const {authorId} = req.params;
-    res.send(`Author ID: ${authorId}`);
-});
+authorRouter.get("/:authorId", getAuthorById)
+
+// authorRouter.get("/:authorId", (req, res) => {
+//     const {authorId} = req.params;
+//     res.send(`Author ID: ${authorId}`);
+// });
 
 module.exports = authorRouter;
