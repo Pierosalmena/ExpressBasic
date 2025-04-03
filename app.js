9ba2734f5d36620fda6b3a4ce5d72c184c110340
pierosalmena <piero.salmena@gmail.com>
2025-04-03 23:13:54 +0200

    Configure EJS view engine and basic usage
    Install EJS and configure the Express app to use it as the
    template engine. Set the 'views' directory for template lookup
    and import the necessary 'path' module.
    
    Create an initial 'index.ejs' template to display passed data.
    Add a route in app.js to render this template with example data.
    
    Implement template reuse with `<%- include() %>` for components like
    a navbar and user list items, passing data to includes. Organize
    view partials into subdirectories (e.g., 'views/users').
    
    Set up static file serving using `express.static` for assets in
    the 'public' directory and link a basic CSS file in the template.
    This covers the core EJS setup and features from the lesson.

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
 
-const authorRouter = require("./routes/authorRouter");
-const bookRouter = require("./routes/bookRouter");
-const indexRouter = require("./routes/indexRouter")
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
-app.use("/authors", authorRouter);
-app.use("/books", bookRouter);
-app.use("/", indexRouter);
+// const authorRouter = require("./routes/authorRouter");
+// const bookRouter = require("./routes/bookRouter");
+// const indexRouter = require("./routes/indexRouter")
+
+// app.use("/authors", authorRouter);
+// app.use("/books", bookRouter);
+// // app.use("/", indexRouter);
+
+const links = [
+    {href: "/", text: "Home"},
+    {href: "about", text: "About"}
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
+app.get("/", (req,res) => {
+    res.render("index", {links: links, users: users})
+})
+
+app.get("/about", (req,res) => {
+    res.render("about", {links: links})
+})
+// app.get("/", (req, res) => {
+//     res.render("index", {links: links });
+// })
+
+// app.use((err, req, res, next) => {
+//     console.error(err);
+//     res.status(err.statusCode || 500).send(err.message);
+// })
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// app.get("/", (req, res) => {
+//     res.render("index", { message: "EJS rocks!", foo: "this is foo"});
+// })
 
 // app.get("/", (req,res) => res.send("Hello, world!"));
 
